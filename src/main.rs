6ded69635c5fe1231139e9f61a7ac5dde932bd17
Maciej Lenartowicz <mcjlnrtwcz@gmail.com>
2018-12-21 12:07:10 +0100

    Stars as an array of points

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,21 @@
 extern crate ggez;
-use ggez::graphics::DrawMode;
+use ggez::graphics::Point2;
 use ggez::*;
 use rand::Rng;
 
 struct MainState {
-    stars: Vec<(f32, f32)>,
+    stars: [Point2; 100],
 }
 
 impl MainState {
     fn new(_ctx: &mut Context) -> GameResult<MainState> {
         // Generate initial stars - background
-        let mut stars: Vec<(f32, f32)> = Vec::new();
-        for _i in 0..100 {
-            stars.push((
-                rand::thread_rng().gen_range(0.0, 800.0),
-                rand::thread_rng().gen_range(0.0, 600.0),
-            ));
+        let mut stars = [Point2::new(0.0, 0.0); 100];
+        for i in 0..100 {
+            stars[i].x = rand::thread_rng().gen_range(0.0, 800.0);
+            stars[i].y = rand::thread_rng().gen_range(0.0, 600.0);
         }
+
         let state = MainState { stars: stars };
         Ok(state)
     }
@@ -29,18 +28,15 @@ impl event::EventHandler for MainState {
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
         graphics::clear(ctx);
-        let mut rect = graphics::Rect::one();
         for star in self.stars.iter_mut() {
-            if star.1 >= 600.0 {
-                star.0 = rand::thread_rng().gen_range(0.0, 800.0);
-                star.1 = 0.0;
+            if star.y >= 600.0 {
+                star.x = rand::thread_rng().gen_range(0.0, 800.0);
+                star.y = 0.0;
             } else {
-                star.1 += 1.0;
+                star.y += 1.0;
             }
-            rect.x = star.0;
-            rect.y = star.1;
-            graphics::rectangle(ctx, DrawMode::Fill, rect)?;
         }
+        graphics::points(ctx, &self.stars, 1.0)?;
         graphics::present(ctx);
         Ok(())
     }
