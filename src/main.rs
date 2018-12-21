795ea581722940cd357e8ff72f850a320de5921a
Maciej Lenartowicz <mcjlnrtwcz@gmail.com>
2018-12-21 23:52:59 +0100

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,46 @@ use ggez::graphics::Point2;
 use ggez::*;
 use rand::Rng;
 
+const BACKGROUND_STARS_NUM: usize = 100;
+
+type BackgroundStars = [Point2; BACKGROUND_STARS_NUM];
+
 struct MainState {
-    stars: [Point2; 100],
+    width: u32,
+    height: u32,
+    stars: BackgroundStars,
 }
 
 impl MainState {
-    fn new(_ctx: &mut Context) -> GameResult<MainState> {
-        // Generate initial stars - background
-        let mut stars = [Point2::new(0.0, 0.0); 100];
+    fn generate_stars(width: u32, height: u32) -> BackgroundStars {
+        let mut stars = [Point2::new(0.0, 0.0); BACKGROUND_STARS_NUM];
         for i in 0..100 {
-            stars[i].x = rand::thread_rng().gen_range(0.0, 800.0);
-            stars[i].y = rand::thread_rng().gen_range(0.0, 600.0);
+            // TODO: Stars proximity
+            stars[i].x = rand::thread_rng().gen_range(0.0, width as f32);
+            stars[i].y = rand::thread_rng().gen_range(0.0, height as f32);
         }
+        stars
+    }
 
-        let state = MainState { stars: stars };
+    fn move_stars(&mut self, width: u32, height: u32) -> &BackgroundStars {
+        for star in self.stars.iter_mut() {
+            if star.y >= height as f32 {
+                star.x = rand::thread_rng().gen_range(0.0, width as f32);
+                star.y = 0.0;
+            } else {
+                star.y += 1.0;
+            }
+        }
+        &self.stars
+    }
+
+    fn new(ctx: &mut Context) -> GameResult<MainState> {
+        let (width, height) = graphics::get_size(ctx);
+        let state = MainState {
+            width: width,
+            height: height,
+            stars: MainState::generate_stars(width, height),
+        };
         Ok(state)
     }
 }
@@ -28,15 +54,7 @@ impl event::EventHandler for MainState {
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
         graphics::clear(ctx);
-        for star in self.stars.iter_mut() {
-            if star.y >= 600.0 {
-                star.x = rand::thread_rng().gen_range(0.0, 800.0);
-                star.y = 0.0;
-            } else {
-                star.y += 1.0;
-            }
-        }
-        graphics::points(ctx, &self.stars, 1.0)?;
+        graphics::points(ctx, self.move_stars(self.width, self.height), 1.0)?;
         graphics::present(ctx);
         Ok(())
     }
