c5d0597848009ac38a9ae7cde242c5624c9f261c
Maciej Lenartowicz <mcjlnrtwcz@gmail.com>
2018-12-21 09:54:03 +0100

    Moving background

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,21 @@ use ggez::graphics::DrawMode;
 use ggez::*;
 use rand::Rng;
 
-struct MainState {}
+struct MainState {
+    stars: Vec<(f32, f32)>,
+}
 
 impl MainState {
     fn new(_ctx: &mut Context) -> GameResult<MainState> {
-        let state = MainState {};
+        // Generate initial stars - background
+        let mut stars: Vec<(f32, f32)> = Vec::new();
+        for _i in 0..100 {
+            stars.push((
+                rand::thread_rng().gen_range(0.0, 800.0),
+                rand::thread_rng().gen_range(0.0, 600.0),
+            ));
+        }
+        let state = MainState { stars: stars };
         Ok(state)
     }
 }
@@ -20,9 +30,15 @@ impl event::EventHandler for MainState {
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
         graphics::clear(ctx);
         let mut rect = graphics::Rect::one();
-        for _i in 0..10 {
-            rect.x = rand::thread_rng().gen_range(0.0, 800.0);
-            rect.y = rand::thread_rng().gen_range(0.0, 600.0);
+        for star in self.stars.iter_mut() {
+            if star.1 >= 600.0 {
+                star.0 = rand::thread_rng().gen_range(0.0, 800.0);
+                star.1 = 0.0;
+            } else {
+                star.1 += 1.0;
+            }
+            rect.x = star.0;
+            rect.y = star.1;
             graphics::rectangle(ctx, DrawMode::Fill, rect)?;
         }
         graphics::present(ctx);
